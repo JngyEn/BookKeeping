2b176075d5b2fe692bee1025fd5a21f8dab842e5
JngyEn <983467683@qq.com>
2024-10-19 09:49:32 +0800

    Feat: 测试账单查询方法

--- backend/src/main/java/com/jngyen/bookkeeping/backend/mapper/BillTransactionMapper.java
+++ backend/src/main/java/com/jngyen/bookkeeping/backend/mapper/BillTransactionMapper.java
@@ -4,6 +4,7 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
 import com.jngyen.bookkeeping.backend.pojo.po.bill.BillTransactionPO;
 
@@ -13,7 +14,7 @@ public interface BillTransactionMapper {
     public int insertNewTransaction(BillTransactionPO billTransactionPO);
     //HACK: 后续结合分页查询改进
     // 查询所有账单交易：按照时间范围
-    public List<BillTransactionPO> queryAllTransactionsByTimeRange(String userUuid, LocalDateTime startDate, LocalDateTime endDate);
+    public List<BillTransactionPO> queryAllTransactionsByTimeRange(String userUuid,  @Param("startTime") LocalDateTime startDate, @Param("endTime") LocalDateTime endDate);
     // 查询某渠道账单：按照时间范围
     // 查询某交易类型账单：按照时间范围
     // 查询收入/支出账单：按照时间范围
